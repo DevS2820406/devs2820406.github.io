08f3002a649ff155a02b81b7330271babbee165b
DevS2820406 <103106371+DevS2820406@users.noreply.github.com>
2022-07-06 21:25:27 +0530

    Update prac9.js

--- prac9.js
+++ prac9.js
@@ -1,3 +1,18 @@
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<body style="background-color:grey ;">
+    <h1 style="text-align:center ;">You can check the consolde for the output of the following js code.</h1>
+   
+</body>
+
 <script>
     // Copying, passing, and comparing by value
     let x = 5
@@ -19,3 +34,5 @@
     increase_count(count)
     console.log('References get themselves passed by value. Output: ' + count)
     </script>
+
+</html>
